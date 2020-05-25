976d4c43ea127a53581429aa14c844ce5a27dfa2
pharia-le <pharia.le@gmail.com>
2020-05-25 07:38:29 -0700

    Create project resource and migrate.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :projects
   resources :teams
   devise_for :users, controllers: {registrations: "registrations", omniauth_callbacks: 'omniauth'}  
   root to: "application#home"
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_142956) do
+ActiveRecord::Schema.define(version: 2020_05_25_143640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.string "session_id", null: false
