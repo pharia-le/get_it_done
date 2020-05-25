d4adc74f7eceea3e31d16b5280b76102738822ea
pharia-le <pharia.le@gmail.com>
2020-05-24 18:08:52 -0700

    Add provider keys and secrets to credentials.yml through terminal.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
 
+    
+    protected
+
+    def update_resource(resource, params)
+      resource.update_without_password(params)
+    end
+    
     private
       def sign_up_params
         params.require(:user).permit(:email, :password, :password_confirmation, :fname, :lname, :age)
