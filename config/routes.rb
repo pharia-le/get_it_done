ec0a59d17856fa57fbb4634ec036d4111b6d2e7a
pharia-le <pharia.le@gmail.com>
2020-05-24 18:50:22 -0700

    Update routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {registrations: 'registrations'}
-
+  devise_for :users, controllers: {registrations: "registrations", omniauth_callbacks: 'omniauth'}  
   root to: "application#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
