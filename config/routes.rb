ee8c507bb28e993b47e8e140b0f8a54cca5e52ab
pharia-le <pharia.le@gmail.com>
2020-05-28 15:41:11 -0700

    Add nested routes for Project > Task.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :tasks
-  resources :projects
+  resources :projects do
+    resources :tasks, only: [:show, :index, :new, :edit]
+  end
   devise_for :users, controllers: {registrations: "registrations", omniauth_callbacks: 'omniauth'}  
   root to: "application#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
