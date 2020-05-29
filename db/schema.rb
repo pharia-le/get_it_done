01f40863dc4f925492f4b2320c8349b6f7f2e1b1
pharia-le <pharia.le@gmail.com>
2020-05-28 17:42:57 -0700

    Add complete boolean to tasks table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_203343) do
+ActiveRecord::Schema.define(version: 2020_05_29_004204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_05_28_203343) do
     t.bigint "project_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "complete", default: false
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
