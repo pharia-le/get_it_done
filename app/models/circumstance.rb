098e9586e48333d44ef41b57a366211c3c21b7fa
pharia-le <pharia.le@gmail.com>
2020-08-12 13:42:59 -0700

    Remove unused/commented code.

--- app/models/circumstance.rb
+++ app/models/circumstance.rb
@@ -1,6 +1,5 @@
 class Circumstance < ApplicationRecord
     has_many :task_circumstances
     has_many :tasks, through: :task_circumstances
-    # default_scope { order("created_at ASC") }
     scope :by_create, -> { order("created_at ASC") }
 end
