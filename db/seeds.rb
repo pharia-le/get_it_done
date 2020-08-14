4077d3c4c386f13fc8c574d5dedb3b5e0b022da0
pharia-le <pharia.le@gmail.com>
2020-08-13 18:00:02 -0700

    Update seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,38 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-# 20.times {
-#     User.create(
-#     fname: Faker::Name.first_name,
-#     lname: Faker::Name.last_name,
-#     email: Faker::Internet.email,
-#     age: rand(50),
-#     password: "password"
-#     )
-# }
-
-# 20.times {
-#     Project.create(
-#     name: Faker::App.name
-#     )
-# }
-
-# 20.times {
-#     Team.create(
-#     name: Faker::Company.name
-#     )
-# }
-
-# Circumstance.create(name: "Home")
-# Circumstance.create(name: "Office")
-# Circumstance.create(name: "Phone")
-# Circumstance.create(name: "Email")
-# Circumstance.create(name: "LOW ENERGY")
-# Circumstance.create(name: "MID ENERGY")
-# Circumstance.create(name: "HIGH ENERGY")
-# Circumstance.create(name: "Urgent")
-# Circumstance.create(name: "5 Min")
-# Circumstance.create(name: "15 Min")
-# Circumstance.create(name: "30 Min")
-# Circumstance.create(name: "60 Min")
+Circumstance.create(name: "Home")
+Circumstance.create(name: "Office")
+Circumstance.create(name: "Phone")
+Circumstance.create(name: "Email")
+Circumstance.create(name: "LOW ENERGY")
+Circumstance.create(name: "MID ENERGY")
+Circumstance.create(name: "HIGH ENERGY")
+Circumstance.create(name: "Urgent")
+Circumstance.create(name: "5 Min")
+Circumstance.create(name: "15 Min")
+Circumstance.create(name: "30 Min")
+Circumstance.create(name: "60 Min")
